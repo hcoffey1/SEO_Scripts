335d53d5c664c81281f4355ae1ecb255f582cd65
Hayden <hjcoffey@wisc.edu>
2025-04-11 10:52:09 -0500

    Update with csv support

---
+++ README.md
@@ -0,0 +1,20 @@
+# convert.py
+
+Supports input data from csv or pdf format.
+
+Example usage:
+
+```
+# Using input pdf
+python convert.py input.pdf input_ref.xlsx
+
+# Using input csv
+python convert.py input.csv input_ref.xlsx
+```
+
+Default output goes to `converted_output.xlsx`.
+Can redirect output by specifing additional argument.
+
+```
+python convert.py input.pdf input_ref.xlsx output_file.xlsx
+```
--- convert.py
+++ convert.py
@@ -9,7 +9,7 @@ def get_pdf_table(file):
     MAGIC_PHRASE = "Well"
     all_tables = []
     with pdfplumber.open(file) as pdf:
-        
+
         for page in pdf.pages:
             tables = page.extract_tables()
             for table in tables:
@@ -30,6 +30,27 @@ def get_pdf_table(file):
 
     return final_df
 
+def process_pdf_table(table_df, input_ref_df, color_ref_df):
+    table_df["Content"] = table_df["Well"].apply(lambda well: get_target_value(well, input_ref_df))
+    table_df["Target"] = table_df["Well"].apply(lambda well: get_target_value(well, color_ref_df))
+
+    table_df = table_df.drop(['Fluor'], axis=1)
+
+    return table_df
+
+def process_csv_table(table_df, input_ref_df, color_ref_df):
+    table_df["Target"] = table_df["Well"].apply(lambda well: get_target_value(well, color_ref_df))
+
+    # Add Content column after Target column
+    col_index = table_df.columns.get_loc('Target') + 1
+    content_values = table_df["Well"].apply(lambda well: get_target_value(well, input_ref_df))
+    table_df.insert(col_index, "Content", content_values)
+
+    # Optional, drop rows with "Undetermined" Cq values.
+    #table_df = table_df.loc[table_df['Cq'] != 'Undetermined']
+
+    return table_df
+
 def get_input_ref_df(file):
     wb = load_workbook(file)
 
@@ -43,43 +64,47 @@ def get_input_ref_df(file):
         if all(value is None for value in l):
             break
         i+=1
-    
+
 
     content_ref_df = pd.DataFrame(val_list[0:i]).drop(index=0).set_index(0)
+
+    #Strip whitespace
+    content_ref_df.index = content_ref_df.index.astype(str).str.strip()
+    #content_ref_df.columns = content_ref_df.columns.astype(str).str.strip()
+
+    # Create color reference table.
     color_ref_df = content_ref_df.copy()
 
-    #Last part to do is add the color information to this dataframe so we can crossreference with other table.
     row = 0
     col = 0
 
+    # Create reference table with theme color values
     for r in ws[2:i]:
         col = 0
         for c in r[1:]:
-            if (color_ref_df.iloc[row,col]): #c.fill.fgColor.theme):
+            if (color_ref_df.iloc[row,col]):
                 color_ref_df.iloc[row,col] = c.fill.fgColor.theme
             else:
                 break
             col += 1
         row += 1
-    
-    #Strip whitespace
-    content_ref_df.index = content_ref_df.index.astype(str).str.strip()
-    #content_ref_df.columns = content_ref_df.columns.astype(str).str.strip()
 
+    # Color row has translations from theme color to string
     color_row = (ws[i+2])
-    
+
+    # Extract key:values of color translation into a table
     color_table = []
     for c in color_row:
         if c.value == None:
             continue
 
         color_table.append([c.fill.fgColor.theme, c.value])
-    
+
+    # Create color dict and apply to color ref df to create final lookup df.
     color_df = (pd.DataFrame(color_table))
     color_df=color_df.set_index(0)
 
     color_match_dict = (color_df[1].to_dict())
-
     color_ref_df = color_ref_df.replace(color_match_dict)
 
     return content_ref_df, color_ref_df
@@ -92,35 +117,42 @@ def get_target_value(well, df_lookup):
     col_number = int(well[1:])
 
     # Get the corresponding lookup value from df_lookup
-    return df_lookup.loc[row_label, col_number]
-
+    if row_label in df_lookup.index and col_number in df_lookup.columns:
+        return df_lookup.loc[row_label, col_number]
+    else:
+        return ""
 
 def main():
 
     if len(sys.argv) < 3:
         print("Usage: python {} input.pdf input_ref.xlsx (optional:) output.xlsx".format(sys.argv[0]))
         return
-    
-    input_pdf_file = sys.argv[1]
+
+    input_file = sys.argv[1]
     input_ref_file = sys.argv[2]
 
-    output_file = "out_hc.xlsx"
+    output_file = "converted_output.xlsx"
 
     if len(sys.argv) == 4:
         output_file = sys.argv[3] 
 
-    pdf_table_df = get_pdf_table(input_pdf_file)
     input_ref_df, color_ref_df = get_input_ref_df(input_ref_file)
 
-    pdf_table_df["Content"] = pdf_table_df["Well"].apply(lambda well: get_target_value(well, input_ref_df))
-    pdf_table_df["Target"] = pdf_table_df["Well"].apply(lambda well: get_target_value(well, color_ref_df))
-
-    pdf_table_df = pdf_table_df.drop(['Fluor'], axis=1)
+    # Process data from either pdf or csv file. 
+    if input_file.endswith('.pdf'):
+        table_df = get_pdf_table(input_file)
+        table_df = process_pdf_table(table_df, input_ref_df, color_ref_df)
+    elif input_file.endswith('.csv'):
+        table_df = pd.read_csv(input_file)
+        table_df = process_csv_table(table_df, input_ref_df, color_ref_df)
+    else:
+        print("Error: Unsupported input file type. Supported: .pdf, .csv")
+        exit()
 
     if os.path.exists(output_file):
-         raise FileExistsError(f"The file '{output_file}' already exists. Will not overwrite.")
+        raise FileExistsError(f"The file '{output_file}' already exists. Will not overwrite.")
 
-    pdf_table_df.to_excel(output_file, index=False)
+    table_df.to_excel(output_file, index=False)
 
 if __name__ == "__main__":
     main()
